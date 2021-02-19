c4ac28b78ac249f11e793c43931f009d163598b4
amrutnrp <60325062+amrutnrp@users.noreply.github.com>
2021-02-20 00:16:12 +0530

    Folder org

--- lua_thingspeak/credentials.lua
+++ lua_thingspeak/credentials.lua
@@ -1,8 +1,8 @@
 -- Credentials
 -- SSID = "Hydra"
 -- PASSWORD = "K5x48Vz3"
-SSID = "apaoq"
-PASSWORD = "1234zzzz"
+SSID = "Disguiser"
+PASSWORD = "8984734521@APA"
 WR_key = '4X1L42JDBX4SKRZH'
 RD_key = '3H0NBBMRQN3HDZGP'
 channel_ID = '688655'
